dbe12cb412d37201c9ea91b0343953622292ee41
Dulaj Nadawa <dulajnadawa@gmail.com>
2021-01-27 01:24:51 +0530

    new routes added

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const cron = require('node-cron');
 
 const app = express();
 
-const task = cron.schedule('1 * * * * *', () => {
-    console.log('running a task every minute at the 5th second');
+let count = 0;
+
+const task = cron.schedule('* 1 * * *', () => {
+    count++;
+    console.log('running a task '+count.toString());
 });
+task.stop();
 
 app.get('/',(req, res) => {
     task.start();
     res.send('Task started succssfully!');
+    console.log('task started');
+});
+
+app.get('/count', (req, res) => {
+    res.send("<h1>"+count.toString()+"</h1>");
 });
 
 app.listen(3000,()=>console.log('server is running'));
